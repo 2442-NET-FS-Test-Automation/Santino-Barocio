208701cc16a5eb4dba490b07cee69bd51a8a019e
Victor <43707720+Hamsuke@users.noreply.github.com>
2026-06-18 14:43:05 -0600

    Uploaded the files
    Uploaded the files since Git rejected every push i commit push i tried

---
+++ Playlist.Domain/PlaylistItem.cs
@@ -0,0 +1,33 @@
+namespace Playlist.Domain;
+
+public abstract class PlaylistItem
+{
+    private static int _nextId = 1;
+    public int Id {get;}
+    public string? Title {get; private set;}
+    public string? Author {get; private set;}
+    public float? Duration {get; private set;}
+    public int TimesPlayed {get; private set;}
+
+
+    protected PlaylistItem(string title, string author, float duration , int timesplayed)
+    {
+        Id = _nextId++;
+        Title = title;
+        Author = author;
+        Duration = duration;
+        TimesPlayed = timesplayed;
+    }
+
+    public void Play() => TimesPlayed += 1;
+
+    public void Play(int times) => TimesPlayed += times;
+
+
+    public virtual string Describe()
+    {
+        return $"{Id}: {Title}";
+    }
+
+
+}
