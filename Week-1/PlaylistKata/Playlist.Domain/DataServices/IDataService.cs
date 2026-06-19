8abab57faf007c32bc65d01f76256fb822e8bf4d
hamusuke <victoriyari@hotmail.com>
2026-06-19 13:25:28 -0600

    Initial part for the http request to Audio-DB-API

---
+++ Week-1/PlaylistKata/Playlist.Domain/DataServices/IDataService.cs
@@ -0,0 +1,8 @@
+namespace Playlist.Domain.DataServices;
+
+public interface IDataService
+{
+    Task<Song?> GetSong(string artist, string song);
+    
+    
+}
---
+++ Week-1/PlaylistKata/Playlist.Domain/DataServices/Indications.md
@@ -0,0 +1,4 @@
+This folder is the main API
+folder here we are managing 
+our terget API calls to process 
+its information 
