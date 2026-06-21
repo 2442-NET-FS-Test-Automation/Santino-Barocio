e208fb16147e92883601b85bbe7dd3ee25b30a6e
Victor <victoriyari@hotmail.com>
2026-06-21 00:11:35 -0600

    Reformat constructor for easier understanding

--- Week-1/PlaylistKata/Playlist.Domain/Song.cs
+++ Week-1/PlaylistKata/Playlist.Domain/Song.cs
@@ -5,7 +5,15 @@ public class Song: PlaylistItem
     public string Genre {get; private set;}
     public string Album {get; private set;}
     
-    public Song(string title, string author, float duration, int timesplayed, string genre, string album) : base(title, author, duration, timesplayed)
+    public Song(
+        string title, 
+        string author, 
+        float duration, 
+        int timesplayed, 
+        string genre, 
+        string album) 
+        : 
+        base(title, author, duration, timesplayed)
     {
         Genre = genre;
         Album = album;
